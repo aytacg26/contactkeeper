59b0aee6e8579c31a78c34d94e1cd65ef814d775
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-08 14:48:26 +0200

    User Model updated

--- models/User.js
+++ models/User.js
@@ -4,15 +4,20 @@ const UserSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
+    minLength: 2,
+    maxLength: 80,
   },
   email: {
     type: String,
     required: true,
     unique: true,
+    maxLength: 80,
   },
   password: {
     type: String,
     required: true,
+    minLength: 6,
+    maxLength: 100, //Hash password will be longer than 30 chars
   },
   createdAt: {
     type: Date,
