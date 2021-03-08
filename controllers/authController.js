b7ff26fe2aaea0d384811179613604e60f5efd02
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-08 16:22:50 +0200

    authController Created

---
+++ controllers/authController.js
@@ -0,0 +1,47 @@
+import User from '../models/User.js';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import config from 'config';
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: 'invalid credentials' });
+    }
+
+    //check password with bcrypt.compare()
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: 'invalid credentials' });
+    }
+
+    //Send token to user:
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+
+    const jwtKey = config.get('JWTSecretKey');
+    const expire = config.get('tokenExpire');
+
+    jwt.sign(payload, jwtKey, { expiresIn: expire }, (error, token) => {
+      if (error) throw error;
+      res.json({ token });
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ message: `Internal Server Error` });
+  }
+};
+
+const authController = Object.freeze({
+  login,
+});
+
+export default authController;
