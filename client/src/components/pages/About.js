89507c8547e87912b69ea381900b52f5a97978d7
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-08 19:54:54 +0200

    About Page Created

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About App</h1>
+      <p className='my-1'>This is a fullstack react app for keeping contacts</p>
+      <p className='bg-dark p'>
+        <strong>Author : </strong> Aytaç Güley
+      </p>
+      <p className='bg-dark p'>
+        <strong>Version : </strong> 1.0.0
+      </p>
+    </div>
+  );
+};
+
+export default About;
