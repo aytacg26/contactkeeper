b597c94fc1453c69e03b1b89f7dc791339f62658
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-08 21:09:56 +0200

    ContactState created with static data

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,66 @@
+import React, { useReducer } from 'react';
+import ContactContext from './contactContext';
+import ContactReducer from './contactReducer';
+import { v4 as uuid } from 'uuid';
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER,
+} from '../types';
+
+const ContactState = (props) => {
+  const initialState = {
+    contacts: [
+      {
+        id: 1,
+        name: 'Aytac Güley',
+        phone: '0548 888 88 26',
+        email: 'aytacg26@gmail.com',
+        contactType: 'personal',
+      },
+      {
+        id: 2,
+        name: 'Steve Jobs',
+        phone: '0549 999 99 18',
+        email: 'sjobs@apple.com',
+        contactType: 'professional',
+      },
+      {
+        id: 3,
+        name: 'Elon Musk',
+        phone: '0549 991 94 18',
+        email: 'elonmusk@tesla.com',
+        contactType: 'professional',
+      },
+    ],
+  };
+
+  const [state, dispatch] = useReducer(ContactReducer, initialState);
+
+  //ACTIONS :
+  //Add Contact
+
+  //Delete Contact
+
+  //Set Current Contact
+
+  //Clear Current Contact
+
+  //Update Contact
+
+  //Filter Contacts
+
+  //Clear Filter
+
+  return (
+    <ContactContext.Provider value={{ contacts: state.contacts }}>
+      {props.children}
+    </ContactContext.Provider>
+  );
+};
+
+export default ContactState;
