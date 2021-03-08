9af568a76626eed48085a9e439abbc0023caf39d
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-08 13:32:50 +0200

    Connection to MongoDB Setup Created

---
+++ config/db.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+import config from 'config';
+
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+  try {
+    mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
+    console.log('MongoDB for ContactKeeper connected...');
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
