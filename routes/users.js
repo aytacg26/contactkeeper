bcdd65ac87c35d15fff9062aecd2c6b018c19df9
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-08 14:49:10 +0200

    User Registration Route Completed

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import UserController from '../controllers/userController.js';
+import { check, validationResult } from 'express-validator';
 
 const UsersRouter = express.Router();
 
@@ -7,8 +9,26 @@ const UsersRouter = express.Router();
  * @description     Register a user
  * @access          Public
  */
-UsersRouter.post('/', (req, res) => {
-  res.send('Registers a user...');
-});
+UsersRouter.post(
+  '/',
+  [
+    check('name', 'name is required').not().isEmpty(),
+    check('name', 'Please enter a valid name').isLength({ min: 2, max: 80 }),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters. Maximum character size must not be more than 30'
+    ).isLength({ min: 6, max: 30 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    UserController.registerUser(req, res);
+  }
+);
 
 export default UsersRouter;
