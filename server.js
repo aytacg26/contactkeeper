a2150bdc57de717df88cf861a7af9f6fc9dd1ab6
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-08 12:19:31 +0200

    Routers added to Server.js

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 import express from 'express';
 import helmet from 'helmet';
+import UsersRouter from './routes/users.js';
+import AuthRouter from './routes/auth.js';
+import ContactsRouter from './routes/contacts.js';
 
 const app = express();
 app.use(helmet());
+
+//Define Routes
+app.use('/api/users', UsersRouter);
+app.use('/api/auth', AuthRouter);
+app.use('/api/contacts', ContactsRouter);
 
 const PORT = process.env.PORT || 5000;
 
