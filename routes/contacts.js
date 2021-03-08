c0669b1a9173f4a93d2e75ac4f5c3bd2fa0bf783
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-08 12:31:54 +0200

    All test Routes added for Contacts

--- routes/contacts.js
+++ routes/contacts.js
@@ -11,4 +11,31 @@ ContactsRouter.get('/', (req, res) => {
   res.send('Registers a user...');
 });
 
+/**
+ * @route           POST api/contacts
+ * @description     Add new contact
+ * @access          Private
+ */
+ContactsRouter.post('/', (req, res) => {
+  res.send('Add a new contact');
+});
+
+/**
+ * @route           PUT api/contacts/:id
+ * @description     Update a contact
+ * @access          Private
+ */
+ContactsRouter.put('/:id', (req, res) => {
+  res.send('update a contact');
+});
+
+/**
+ * @route           DELETE api/contacts/:id
+ * @description     Delete a contact
+ * @access          Private
+ */
+ContactsRouter.delete('/:id', (req, res) => {
+  res.send('delete a contact');
+});
+
 export default ContactsRouter;
