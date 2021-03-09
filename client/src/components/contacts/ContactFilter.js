011f7789c3f68038b19d794115a9cdc13aed0736
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-09 21:03:01 +0200

    ContactFilter Component Created

---
+++ client/src/components/contacts/ContactFilter.js
@@ -0,0 +1,35 @@
+import React, { useContext, useRef, useEffect } from 'react';
+import ContactContext from '../../context/contact/contactContext';
+
+const ContactFilter = () => {
+  const contactContext = useContext(ContactContext);
+  const { filterContacts, clearFilter, filtered } = contactContext;
+  const text = useRef('');
+
+  useEffect(() => {
+    if (filtered === null) {
+      text.current.value = '';
+    }
+  }, [filtered, text.current.value]);
+
+  const onChange = () => {
+    if (text.current.value !== '') {
+      filterContacts(text.current.value);
+    } else {
+      clearFilter();
+    }
+  };
+
+  return (
+    <form>
+      <input
+        type='text'
+        ref={text}
+        placeholder='Filter Contacts'
+        onChange={onChange}
+      />
+    </form>
+  );
+};
+
+export default ContactFilter;
