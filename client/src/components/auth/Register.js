e6c6a66cb139b1fa6872b18f24f1719f29e4fd8a
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-09 22:29:53 +0200

    Register Component Created

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+
+const Register = () => {
+  const [user, setUser] = useState({
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const { name, email, password, confirmPassword } = user;
+
+  const handleChange = (e) =>
+    setUser({ ...user, [e.target.name]: e.target.value });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Register Submit');
+  };
+
+  return (
+    <div className='form-container'>
+      <h1>
+        Account <span className='text-primary'>Register</span>
+      </h1>
+      <form onSubmit={handleSubmit}>
+        <div className='form-group'>
+          <label htmlFor='name'>Name</label>
+          <input type='text' name='name' value={name} onChange={handleChange} />
+        </div>
+
+        <div className='form-group'>
+          <label htmlFor='email'>Email</label>
+          <input
+            type='email'
+            name='email'
+            value={email}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className='form-group'>
+          <label htmlFor='password'>Password</label>
+          <input
+            type='password'
+            name='password'
+            value={password}
+            onChange={handleChange}
+          />
+        </div>
+
+        <div className='form-group'>
+          <label htmlFor='confirmPassword'>Confirm Password</label>
+          <input
+            type='password'
+            name='confirmPassword'
+            value={confirmPassword}
+            onChange={handleChange}
+          />
+        </div>
+        <input
+          type='submit'
+          value='Register'
+          className='btn btn-primary btn-block'
+        />
+      </form>
+    </div>
+  );
+};
+
+export default Register;
