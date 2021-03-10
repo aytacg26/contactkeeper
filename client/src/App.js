be5233ebf84144c60da69da057ee7d2c70c83c9f
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-10 23:55:07 +0200

    setAuthToken is called in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,11 @@ import Navbar from './components/layout/Navbar';
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
 import AlertState from './context/alert/AlertState';
+import setAuthToken from './utils/setAuthToken';
+
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
 const App = () => {
   return (
