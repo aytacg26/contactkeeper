683439e2bd2996e2fe51d20b29bdb825583141a1
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-10 18:08:34 +0200

    AlertState added to App

--- client/src/App.js
+++ client/src/App.js
@@ -8,26 +8,29 @@ import './App.css';
 import Navbar from './components/layout/Navbar';
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
+import AlertState from './context/alert/AlertState';
 
 const App = () => {
   return (
-    <AuthState>
-      <ContactState>
-        <Router>
-          <Fragment>
-            <Navbar />
-            <div className='container'>
-              <Switch>
-                <Route exact path='/' component={Home} />
-                <Route exact path='/about' component={About} />
-                <Route exact path='/register' component={Register} />
-                <Route exact path='/login' component={Login} />
-              </Switch>
-            </div>
-          </Fragment>
-        </Router>
-      </ContactState>
-    </AuthState>
+    <AlertState>
+      <AuthState>
+        <ContactState>
+          <Router>
+            <Fragment>
+              <Navbar />
+              <div className='container'>
+                <Switch>
+                  <Route exact path='/' component={Home} />
+                  <Route exact path='/about' component={About} />
+                  <Route exact path='/register' component={Register} />
+                  <Route exact path='/login' component={Login} />
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
+        </ContactState>
+      </AuthState>
+    </AlertState>
   );
 };
 
