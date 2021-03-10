b6d9acc227efbdb29317aaf5fa8164d52c0ef654
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-10 23:56:37 +0200

    loading auth user added to Home Page

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import Contacts from '../contacts/Contacts';
 import ContactForm from '../contacts/ContactForm';
 import ContactFilter from '../contacts/ContactFilter';
+import authContext from '../../context/auth/authContext';
 
 const Home = () => {
+  const { loadUser } = useContext(authContext);
+
+  useEffect(() => {
+    loadUser();
+
+    //eslint-disable-next-line
+  }, []);
+
   return (
     <div className='grid-2'>
       <div>
