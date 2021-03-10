8abdc5b9b70da37bf789913fca2eb47a013d7df6
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-10 20:52:25 +0200

    Alerts Component Added

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Home from './components/pages/Home';
 import About from './components/pages/About';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alerts from './components/layout/Alerts';
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import ContactState from './context/contact/ContactState';
@@ -19,6 +20,7 @@ const App = () => {
             <Fragment>
               <Navbar />
               <div className='container'>
+                <Alerts />
                 <Switch>
                   <Route exact path='/' component={Home} />
                   <Route exact path='/about' component={About} />
