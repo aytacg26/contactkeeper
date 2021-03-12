9bddb5023e519b78a260f189d3d5a60ed1af876d
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-12 14:08:19 +0200

    CONTACT_ERROR added

--- client/src/context/types.js
+++ client/src/context/types.js
@@ -1,5 +1,6 @@
 export const ADD_CONTACT = 'ADD_CONTACT';
 export const DELETE_CONTACT = 'DELETE_CONTACT';
+export const CONTACT_ERROR = 'CONTACT_ERROR';
 export const SET_CURRENT = 'SET_CURRENT';
 export const CLEAR_CURRENT = 'CLEAR_CURRENT';
 export const UPDATE_CONTACT = 'UPDATE_CONTACT';
