57716827cae9cad2064867c477a48c8011aee4dd
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-12 14:10:01 +0200

    email validation updated

--- client/src/utils/validations.js
+++ client/src/utils/validations.js
@@ -1,5 +1,5 @@
 export const emailValidation = (email) => {
-  const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
   return regex.test(email.toLowerCase());
 };
