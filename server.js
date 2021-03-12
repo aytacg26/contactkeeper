27f7c2ad3c5d2d0ae0e26093f0da2301f013c9f9
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-12 16:57:09 +0200

    Server.js and package.json prepared for deployment

--- server.js
+++ server.js
@@ -4,9 +4,12 @@ import UsersRouter from './routes/users.js';
 import AuthRouter from './routes/auth.js';
 import ContactsRouter from './routes/contacts.js';
 import connectDB from './config/db.js';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const app = express();
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 //Connect Database:
 connectDB();
 
@@ -20,6 +23,14 @@ app.use(helmet());
 app.use('/api/users', UsersRouter);
 app.use('/api/auth', AuthRouter);
 app.use('/api/contacts', ContactsRouter);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
